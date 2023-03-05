abc0a70acffa0ed0d1bc7a543fe07b7365641254
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-05 11:05:59 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,5 +2,30 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+function HelloWorld() {
+  return (
+    <article>
+      <h1>Hello World</h1>
+      <p>
+        ipsum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto
+        quam aperiam sint, beatae necessitatibus pariatur sed atque placeat.
+        Deleniti, velit tempora. Repellendus quos quia assumenda delectus ab
+        esse quas ducimus?
+      </p>
+      <h1>Hello Again</h1>
+      <p>
+        ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+        voluptas illum nostrum ab itaque! Debitis voluptatibus, dolorem aliquam
+        iste est accusamus soluta, ducimus laborum dolores ratione quae quasi
+        porro? Perferendis!
+      </p>
+    </article>
+  );
 }
